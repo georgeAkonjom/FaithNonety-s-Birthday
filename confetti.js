96f1a2d28c8485edbe59d4b0286a5625f53286e7
samuel-george <georgeakonjom003@gmail.com>
2024-09-30 00:35:37 +0100

    Initial commit

---
+++ confetti.js
@@ -0,0 +1,47 @@
+/**
+https://github.com/matteobruni/tsparticles
+https://particles.js.org
+https://confetti.js.org
+*/
+const duration = 60 * 60 * 1000,
+	animationEnd = Date.now() + duration,
+	defaults = {
+		startVelocity: 30,
+		spread: 360,
+		ticks: 20,
+		zIndex: 1,
+	};
+
+function randomInRange(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+const interval = setInterval(function () {
+	const timeLeft = animationEnd - Date.now();
+
+	if (timeLeft <= 0) {
+		return clearInterval(interval);
+	}
+
+	const particleCount = 5 * (timeLeft / duration);
+
+	// since particles fall down, start a bit higher than random
+	confetti(
+		Object.assign({}, defaults, {
+			particleCount,
+			origin: {
+				x: randomInRange(0.1, 0.3),
+				y: Math.random() - 0.2,
+			},
+		})
+	);
+	confetti(
+		Object.assign({}, defaults, {
+			particleCount,
+			origin: {
+				x: randomInRange(0.7, 0.9),
+				y: Math.random() - 0.2,
+			},
+		})
+	);
+}, 250);
